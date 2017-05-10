44079ef2f47c9d0382bb6a4b8fad2a5fede15f88
28kayak <kaya.ota@sjsu.edu>
2017-05-10 12:18:05 -0700

    smll edit + change ui for game activity

--- CS161-Spy/app/src/main/java/com/example/spy/Game.java
+++ CS161-Spy/app/src/main/java/com/example/spy/Game.java
@@ -11,8 +11,11 @@ import android.content.Intent;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.GlideDrawableImageViewTarget;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -54,6 +57,11 @@ public class Game extends AppCompatActivity implements View.OnClickListener {
         user = FirebaseAuth.getInstance().getCurrentUser();
         database = FirebaseDatabase.getInstance();
         myRef = database.getReference();
+        //load gif files
+        ImageView imageView = (ImageView) findViewById(R.id.change_spy);
+        GlideDrawableImageViewTarget imageViewTarget = new GlideDrawableImageViewTarget(imageView);
+        Glide.with(this).load(R.raw.change_spy).into(imageViewTarget);
+
 
         //Views
         role = (TextView) findViewById(R.id.label_role);
