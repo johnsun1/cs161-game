2a5f971b2449646d54f74b4544f1f5578441d850
baconlover51 <vuueric@yahoo.com>
2017-12-10 22:08:31 -0800

    Revert "compass sensor added"
    This reverts commit 41ce30d9150b70363aa8aefb2a4d3bfbfbb4f911.

--- CS161-Spy/app/src/main/java/com/example/spy/MainActivity.java
+++ CS161-Spy/app/src/main/java/com/example/spy/MainActivity.java
@@ -1,20 +1,13 @@
 package com.example.spy;
 
-import android.content.res.Configuration;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
 import android.support.annotation.NonNull;
 
 import android.content.Intent;
 
 import android.util.Log;
 
-import android.view.animation.Animation;
-import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
 import android.widget.Toast;
 import android.widget.EditText;
@@ -35,29 +28,21 @@ import com.google.android.gms.tasks.Task;
 import java.io.IOException;
 import java.io.InputStream;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener, SensorEventListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private EditText field_email;
     private EditText field_password;
-    private ImageView imageView3;
     private static final String TAG = "MainActivity";
 
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
-
-    private float[] mGravity = new float[3];
-    private float[] mGeomagnetic = new float[3];
-    private float azimuth = 0f;
-    private float currentAzimuth = 0f;
-    private SensorManager mSensorManager;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //load image for login screen from asset folder
-        imageView3= (ImageView)findViewById(R.id.spy2);
-        mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        ImageView imageView3 = (ImageView)findViewById(R.id.spy2);
+
         try {
             InputStream istream = getResources().getAssets().open("spy2.jpg");
             Bitmap bitmap = BitmapFactory.decodeStream(istream);
@@ -94,34 +79,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         //Request focus on email field
         field_email.requestFocus();
 
-    }
-
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        if(newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE)
-        {
-            Toast.makeText(this,"our app is running in landscape.", Toast.LENGTH_LONG).show();
-        }
-        else
-        {
-            Toast.makeText(this,"our app is running in portrait.", Toast.LENGTH_LONG).show();
-        }
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        mSensorManager.registerListener(this,mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD),
-                SensorManager.SENSOR_DELAY_GAME);
-        mSensorManager.registerListener(this,mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
-                SensorManager.SENSOR_DELAY_GAME);
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        mSensorManager.unregisterListener(this);
     }
 
     @Override
@@ -184,51 +141,5 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         } else if (i == R.id.button_register) {
             createAccount(field_email.getText().toString(), field_password.getText().toString());
         }
-    }
-
-    @Override
-    public void onSensorChanged(SensorEvent sensorEvent) {
-        final float alpha = 0.97f;
-        synchronized (this)
-        {
-            if(sensorEvent.sensor.getType()== Sensor.TYPE_ACCELEROMETER)
-            {
-                mGravity[0] = alpha*mGravity[0]+(1-alpha)*sensorEvent.values[0];
-                mGravity[1] = alpha*mGravity[1]+(1-alpha)*sensorEvent.values[1];
-                mGravity[2] = alpha*mGravity[2]+(1-alpha)*sensorEvent.values[2];
-            }
-            if(sensorEvent.sensor.getType()== Sensor.TYPE_MAGNETIC_FIELD)
-            {
-                mGeomagnetic[0] = alpha*mGeomagnetic[0]+(1-alpha)*sensorEvent.values[0];
-                mGeomagnetic[1] = alpha*mGeomagnetic[1]+(1-alpha)*sensorEvent.values[1];
-                mGeomagnetic[2] = alpha*mGeomagnetic[2]+(1-alpha)*sensorEvent.values[2];
-            }
-
-            float R[] = new float[9];
-            float I[] = new float[9];
-            boolean success = SensorManager.getRotationMatrix(R,I,mGravity,mGeomagnetic);
-            if(success)
-            {
-                float orientation[] = new float[3];
-                SensorManager.getOrientation(R, orientation);
-                azimuth = (float)Math.toDegrees(orientation[0]);
-                azimuth = (azimuth+360)%360;
-
-                Animation anim = new RotateAnimation(-currentAzimuth,-azimuth,Animation.RELATIVE_TO_SELF,0.5f,Animation.RELATIVE_TO_SELF,0.5f);
-                currentAzimuth = azimuth;
-
-                anim.setDuration(500);
-                anim.setRepeatCount(0);
-                anim.setFillAfter(true);
-
-                imageView3.startAnimation(anim);
-            }
-
-        }
-    }
-
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int i) {
-
     }
 }
